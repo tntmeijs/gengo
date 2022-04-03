0290392ffadcd0d9995c90c1411864cc38c3d771
tntmeijs <tntmeijs@gmail.com>
2022-04-03 22:18:35 +0200

    feat: use GoRoutines to speed up the renderer

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"log"
 	"math"
+	"runtime"
+	"sync"
+	"time"
 
 	. "github.com/tntmeijs/gengo/mathematics"
 	. "github.com/tntmeijs/gengo/scene"
@@ -11,6 +15,10 @@ import (
 // =============================================================================================================================
 // =============================================================================================================================
 // =============================================================================================================================
+
+// System
+const GoRoutinesPerAvailableCpu = 1
+const AverageNumberOfTasksPerWorker = 2
 
 // Output image information
 const ImageResolutionX = 640
@@ -42,9 +50,32 @@ var surfaceColor = Color{Red: 26, Green: 188, Blue: 156, Alpha: 255}
 // =============================================================================================================================
 // =============================================================================================================================
 
+var scene = NewScene(sceneSDF)
+var camera = NewCamera(cameraPosition, cameraLookAt, CameraNearPlane, CameraFarPlane)
+
+// =============================================================================================================================
+// =============================================================================================================================
+// =============================================================================================================================
+
+// Represents a render task
+type RenderTask struct {
+	StartRow, RowCount int
+}
+
+// Represents the result of a render task
+type RenderResult struct {
+	StartRow, RowCount int
+	Pixels             []Color
+}
+
+// Log the time since the start time
+func trackTime(start time.Time, name string) {
+	log.Printf("%s took %s", name, time.Since(start))
+}
+
 // Entire scene represented as a signed distance function
 func sceneSDF(point Vec3) float64 {
-	return MandelbulbSDF(point, 7, 8, 5.0)
+	return MandelbulbSDF(point, 10, 8, 5.0)
 }
 
 // Simple Blinn-Phong lighting model
@@ -65,13 +96,30 @@ func calculatePixelColor(surfaceInfo SurfaceHitInfo, camera Camera) Color {
 	return ColorFromNormalizedVec3(outputColor)
 }
 
-// Application entry point
-func main() {
-	scene := NewScene(sceneSDF)
-	image := NewPngImage(ImageResolutionX, ImageResolutionY, ImageFileName)
-	camera := NewCamera(cameraPosition, cameraLookAt, CameraNearPlane, CameraFarPlane)
+// Worker GoRoutine that fetches a render task from the queue and executes it
+func renderWorker(waitGroup *sync.WaitGroup, pendingWorkQueue <-chan RenderTask, finishedWorkQueue chan<- RenderResult, id int) {
+	defer waitGroup.Done()
 
-	for y := 0; y < ImageResolutionY; y++ {
+	for {
+		// Block until either a task is found, or until the channel has been closed
+		task, more := <-pendingWorkQueue
+
+		if more {
+			log.Println("Worker", id, "started on a task from the pending work queue - remaining tasks:", len(pendingWorkQueue))
+			finishedWorkQueue <- render(task)
+		} else {
+			log.Println("Worker", id, "ran out of tasks - shutting down GoRoutine now")
+			break
+		}
+	}
+}
+
+// Render the scene
+func render(task RenderTask) RenderResult {
+	renderResult := RenderResult{task.StartRow, task.RowCount, make([]Color, task.RowCount*ImageResolutionX)}
+
+	pixelIndex := 0
+	for y := task.StartRow; y < task.StartRow+task.RowCount; y++ {
 		for x := 0; x < ImageResolutionX; x++ {
 			ray := camera.GenerateRayForPixelCenter(x, y, ImageResolutionX, ImageResolutionY)
 			pixelColor := Color{Red: 0, Green: 0, Blue: 0, Alpha: 0}
@@ -82,7 +130,70 @@ func main() {
 				pixelColor = calculatePixelColor(hitInfo, camera)
 			}
 
-			image.SetPixelColor(x, y, pixelColor)
+			renderResult.Pixels[pixelIndex] = pixelColor
+			pixelIndex++
+		}
+	}
+
+	return renderResult
+}
+
+// Wait for all worker GoRoutines to finish and close the finished work queue afterwards
+func waitForWorkers(waitGroup *sync.WaitGroup, finishedWorkQueue chan<- RenderResult) {
+	waitGroup.Wait()
+	close(finishedWorkQueue)
+}
+
+// Application entry point
+func main() {
+	defer trackTime(time.Now(), "Render")
+
+	image := NewPngImage(ImageResolutionX, ImageResolutionY, ImageFileName)
+
+	// Start all workers
+	waitGroup := sync.WaitGroup{}
+	workerCount := (runtime.NumCPU() - 1) * GoRoutinesPerAvailableCpu
+	totalNumberOfTasks := AverageNumberOfTasksPerWorker * workerCount
+
+	pendingWorkQueue := make(chan RenderTask, totalNumberOfTasks)
+	finishedWorkQueue := make(chan RenderResult, totalNumberOfTasks)
+
+	log.Println("Found", runtime.NumCPU(), "CPUs, this will result in", workerCount, "worker GoRoutines")
+	log.Println("Each worker has an average of", AverageNumberOfTasksPerWorker, "tasks")
+	log.Println("This results in a total of", totalNumberOfTasks, "tasks for all worker GoRoutines combined")
+	log.Println("Workload will be divided into", totalNumberOfTasks, "smaller render tasks")
+	log.Println("Output image will have a final resolution of", ImageResolutionX, "x", ImageResolutionY, "pixels")
+
+	for i := 0; i < workerCount; i++ {
+		waitGroup.Add(1)
+		go renderWorker(&waitGroup, pendingWorkQueue, finishedWorkQueue, i)
+	}
+
+	// Generate all jobs
+	for i := 0; i < totalNumberOfTasks; i++ {
+		rowsToRenderPerJob := int(math.Floor(float64(ImageResolutionY) / float64(totalNumberOfTasks)))
+		startRow := i * rowsToRenderPerJob
+		rowCount := int(math.Max(float64(rowsToRenderPerJob), float64(ImageResolutionY-startRow)))
+
+		pendingWorkQueue <- RenderTask{startRow, rowCount}
+	}
+
+	// No more jobs will be added at this point
+	close(pendingWorkQueue)
+
+	// Wait until the scene has been rendered
+	waitForWorkers(&waitGroup, finishedWorkQueue)
+
+	// Keep reading render results from the queue as the worker GoRoutines slowly finish their work
+	for result := range finishedWorkQueue {
+		pixelIndex := 0
+
+		// Save each render chunk in the final output file
+		for y := result.StartRow; y < result.StartRow+result.RowCount; y++ {
+			for x := 0; x < ImageResolutionX; x++ {
+				image.SetPixelColor(x, y, result.Pixels[pixelIndex])
+				pixelIndex++
+			}
 		}
 	}
 
